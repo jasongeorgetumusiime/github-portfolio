28b8cb1b26da62796e67793b0bd8691db0d0b8bc
Jason Tumusiime <jasongeorgetumusiime@gmail.com>
2021-01-06 12:25:08 +0300

    initial version

---
+++ src/containers/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components'
+import Header from '../components/Header/Header'
+import Profile from './Profile'
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", 
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", 
+      "Helvetica Neue", san-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <Header />
+        <Profile />
+      </AppWrapper>
+    </>
+  );
+}
+
+export default App;
